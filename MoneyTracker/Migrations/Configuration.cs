6238c0ce71957764cb9d66a02cadeffe2a08eaaa
AtanasT <atodorovski@insynctive.com>
2015-09-12 22:29:15 +0200

    Fix

--- MoneyTracker/Migrations/Configuration.cs
+++ MoneyTracker/Migrations/Configuration.cs
@@ -13,11 +13,18 @@ namespace MoneyTracker.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            
         }
 
         protected override void Seed(MoneyTracker.Models.MoneyTrackerContext context)
         {
+            MoneyTrackerContext ctx = new MoneyTrackerContext();
+            ctx.Categories.AddOrUpdate(new Category { CategoryName = "Shopping", CreateDate = DateTime.Now });
+            ctx.Categories.AddOrUpdate(new Category { CategoryName = "Food", CreateDate = DateTime.Now });
+            ctx.Categories.AddOrUpdate(new Category { CategoryName = "Travel", CreateDate = DateTime.Now });
+
+            ctx.SaveChanges();
+            ctx.Dispose();
+
             if(!context.Users.Any(u => u.UserName == "Admin"))
             {
                 var store = new UserStore<User>(context);
